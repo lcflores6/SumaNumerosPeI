517a7c0b3a680d6284743dafc7d6f5bbce52e53c
Lenin Flores <lcflores6@espe.edu.ec>
2025-10-13 12:05:34 -0500

    v2.0

--- SumaNumerosPares.c
+++ SumaNumerosPares.c
@@ -3,23 +3,26 @@
 
 int main ()
 {
-    int n, num, i, suma = 0;
+    int n, num, i, sumapares = 0, sumaimpares = 0;
     printf("Ingrese la cantidad de numeros que desea ingresar: ");
     scanf("%d", &n);
-
     int u[n];
     for (i=0; i<n; i++)
     {
         scanf("%d", &u[i]);
     }
-
     for (i=0; i<n; i++)
     {
         if (u[i]%2 == 0)
         {
-            suma = suma + u[i];
+            sumapares = sumapares + u[i];
+        }
+        else if (u[i]%2 != 0)
+        {
+            sumaimpares = sumaimpares + u[i];
         }
     }
-    printf("La suma de los numeros pares es: %d", suma);
+    printf("La suma de los numeros pares es: %d \n", sumapares);
+    printf("La suma de los numeros impares es: %d", sumaimpares);
 
 }
